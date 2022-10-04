3752d1f99163523148fed60b431cf334b7b7db83
hammasrana <114981680+hammasrana@users.noreply.github.com>
2022-10-04 12:36:49 +0500

    Update post.js

--- Models/post.js
+++ Models/post.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-// const { stringify } = require("querystring");
-// const validator = require("validator");
+const { stringify } = require("querystring");
 
 const health = new mongoose.Schema({
   name: {
