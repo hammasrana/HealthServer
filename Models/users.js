f164c3c2a2ea9afee8dd4e04040345b70386e95e
Kumail Latif <56933027+muhammad-kumail@users.noreply.github.com>
2022-11-11 00:27:23 +0500

    Add files via upload

---
+++ Models/users.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const validator = require('validator');
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, required: true, unique: true},
+    email: {type: String, validate: [validator.isEmail, 'invalid email'], required: true},
+    password: {type: String, default: ''},
+    degree: {type: String, default: 'none'},
+    speciality: {type: String, default: 'none'},
+    type: {type: String, required: true}
+});
+const Users = new mongoose.model('Users', UserSchema);
+module.exports = Users;
